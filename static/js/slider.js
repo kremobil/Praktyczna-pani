cebb221d14fa21843a3a85c892facf098d607a7e
kremobil <kremobil@wp.pl>
2021-06-30 22:06:51 +0200

    slider

--- static/js/slider.js
+++ static/js/slider.js
@@ -8,6 +8,8 @@ function sleep(milliseconds) {
 var slide_number = 1
 var photo = "url('../static/img/slide" + slide_number + ".jpg')"
 const slide = document.getElementsByClassName("slide")[0]
+const slider = document.getElementsByClassName("slider")[0]
+
 slide.style.backgroundImage = photo;
 console.log(photo)
 
@@ -22,17 +24,34 @@ function slide_change(increase){
     if (slide_number == 6){
     slide_number = 1
     }
-    console.log(slide_number)
     photo = "url('../static/img/slide" + slide_number + ".jpg')"
-    const slide = document.getElementsByClassName("slide")[0]
-    slide.style.backgroundImage = photo;
-    for (let i = 0; i < 100; i++){
+    slider.style.backgroundImage = photo;
 
-        setTimeout(printI(i),1000);
+    let i = 1;
 
+    function increment() {
+       i = i - 0.01;
+       slide.style.opacity = i;
     }
+
+    let timer = setTimeout(function myTimer() {
+       increment();
+
+       timer = setTimeout(myTimer, 10);
+    }, 10);
+
+    setTimeout(() => {
+    slide.style.opacity = 0;
+    clearTimeout(timer); }, 1000);
+
+    opacityslide()
+
 }
 
+function opacityslide(){
+    slide.style.backgroundImage = photo;
+    slide.style.opacity = 1;
+}
 //
 //const slide = document.getElementsByClassName("slide")[0]
 //slide.style.backgroundImage = photo;
